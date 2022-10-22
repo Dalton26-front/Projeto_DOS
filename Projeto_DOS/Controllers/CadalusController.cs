622b32a88304e67ff4b46211529da96926f579e5
Dalton26-front <dtcastro2013@gmail.com>
2022-10-21 21:31:25 -0300

    Troca do Logo

--- Projeto_DOS/Controllers/CadalusController.cs
+++ Projeto_DOS/Controllers/CadalusController.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Projeto_DOS.Data;
 using Projeto_DOS.Models;
 using Projeto_DOS.Services;
-using Projeto_DOS.Models.ViewModels;
 using SalesWebMvc.Services.Exceptions;
 using X.PagedList;
 
